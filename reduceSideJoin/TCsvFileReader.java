ff3aa2ea57a2da19b955ba7004b77a6c70aa5bb3
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-10-16 02:15:15 +0300

    Scripted auto-commit on change (2019-10-16 02:15:15) by gitwatch.sh

--- reduceSideJoin/TCsvFileReader.java
+++ reduceSideJoin/TCsvFileReader.java
@@ -1,4 +1,12 @@
 package reduceSideJoin;
 
+import java.util.ArrayList;
+import org.apache.hadoop.io.Text;
+
 public class TCsvFileReader {
+    private ArrayList<ArrayList<String>> flightData;
+
+    public TCsvFileReader(Text text) {
+        String fileData = text.toString();
+    }
 }
