c27a1436fc0de40be31583b36ccea733b8630c97
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-10-16 04:46:44 +0300

    Scripted auto-commit on change (2019-10-16 04:46:44) by gitwatch.sh

--- reduceSideJoin/TAirportIdPart.java
+++ reduceSideJoin/TAirportIdPart.java
@@ -6,6 +6,6 @@ import org.apache.hadoop.io.Text;
 public class TAirportIdPart extends Partitioner<TAirportKeyComparable, Text>
 {
     public int getPartition(TAirportKeyComparable key, Text text, int numTasks) {
-        return key.getHa
+        return key.hashCode() % numTasks;
     }
 }
