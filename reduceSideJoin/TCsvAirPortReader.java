e021314de7bd6ea9d1d3539d07049382fe8f4a1c
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-10-16 03:21:15 +0300

    Scripted auto-commit on change (2019-10-16 03:21:15) by gitwatch.sh

--- reduceSideJoin/TCsvAirPortReader.java
+++ reduceSideJoin/TCsvAirPortReader.java
@@ -10,30 +10,9 @@ public class TCsvAirPortReader {
     public TCsvAirPortReader(Text text) {
         String fileData = text.toString();
         String[] lines = fileData.split("\n");
-        flightData = new ArrayList<ArrayList<String>>();
+        airportData = new ArrayList<String>();
         for (int k = 1; k < lines.length; k++) {
-            int count = 0;
-            String line = lines[k];
-            ArrayList<String> dataLine = new ArrayList<String>();
-            for (int i = 0; i < line.length(); i++) {
-                if (line.charAt(i) == ',') {
-                    count++;
-                }
-                if (count == 13) {
-                    i++;
-                    dataLine.add(line.substring(i, i+5));
-                    i+=5;
-                }
-                if (count == 15) {
-                    i++;
-                    String delay = "";
-                    while (line.charAt(i) != ',') {
-                        delay += line.charAt(i);
-                    }
-                    dataLine.add(delay);
-                    break;
-                }
-            }
+            String dataLine = lines[k].substring(1, 6);
             airportData.add(dataLine);
         }
     }
@@ -43,10 +22,6 @@ public class TCsvAirPortReader {
     }
 
     public String getId(int i) {
-        return airportData.get(i).get(0);
-    }
-
-    public String getDelay(int i) {
-        return airportData.get(i).get(1);
+        return airportData.get(i);
     }
 }
