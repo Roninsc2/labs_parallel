1aefb940cff16fc099b68507259154c0d4a8577a
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-10-16 04:59:20 +0300

    Scripted auto-commit on change (2019-10-16 04:59:20) by gitwatch.sh

--- reduceSideJoin/TDelayApp.java
+++ reduceSideJoin/TDelayApp.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TDelayApp {
