7a533681c43658591a01f63c2766f26ce34c3cbb
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-10-16 04:13:51 +0300

    Scripted auto-commit on change (2019-10-16 04:13:51) by gitwatch.sh

--- reduceSideJoin/TAirportReducer.java
+++ reduceSideJoin/TAirportReducer.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.io.LongWritable;
 
 public class TAirportReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
     @Override
-    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    protected void reduce(TAirportKeyComparable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
        long count=0;
        Iterator iter = values.iterator();
        while(iter.hasNext()) {
