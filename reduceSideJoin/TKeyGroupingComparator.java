ec5bee0749ee34d9ce96080b21bd261bf31f7c32
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-10-16 04:49:10 +0300

    Scripted auto-commit on change (2019-10-16 04:49:10) by gitwatch.sh

--- reduceSideJoin/TKeyGroupingComparator.java
+++ reduceSideJoin/TKeyGroupingComparator.java
@@ -1,4 +1,45 @@
 package reduceSideJoin;
 
-public class TKeyGroupingComparator {
+import org.apache.hadoop.io.WritableComparable;
+import  java.io.DataInput;
+import  java.io.DataOutput;
+import  java.io.IOException;
+
+
+public class TAirportKeyComparable implements WritableComparable<TAirportKeyComparable>
+{
+    private int key;
+    private int type;
+
+    public  TAirportKeyComparable(int a, int b) {
+        key = a;
+        type = b;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(key);
+        out.writeLong(type);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        key = in.readInt();
+        type = in.readInt();
+    }
+
+    public int compareTo(TAirportKeyComparable o) {
+        int diff = Integer.compare(this.key, o.key);
+        if (diff == 0) {
+            return Integer.compare(this.type, o.type);
+        }
+        return diff;
+    }
+
+    public int hashCode() {
+        return Integer.hashCode(key);
+    }
 }
