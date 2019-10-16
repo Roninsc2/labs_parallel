9c21374206ee932ae1949dac0594d1fc54b91e1d
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-10-16 04:07:35 +0300

    Scripted auto-commit on change (2019-10-16 04:07:35) by gitwatch.sh

--- reduceSideJoin/TAirportMapper.java
+++ reduceSideJoin/TAirportMapper.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class TAirportMapper extends Mapper<LongWritable, Text, TAirportKeyComparable> {
     @Override
-    protected void map(TAirportKeyComparable key, Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Context context) throws IOException, InterruptedException {
         TCsvAirPortReader reader = new TCsvAirPortReader(value);
         for (int i = 0; i < reader.getDataSize(); i++) {
             int key_val = Integer.parseInt(reader.getId(i));
