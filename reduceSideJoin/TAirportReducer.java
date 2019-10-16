55d8a6c7840b6f24b1604d576c8e7143e95163bc
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-10-16 04:17:10 +0300

    Scripted auto-commit on change (2019-10-16 04:17:10) by gitwatch.sh

--- reduceSideJoin/TAirportReducer.java
+++ reduceSideJoin/TAirportReducer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import java.nio.channels.FileLock;
 import java.util.Iterator;
 import org.apache.hadoop.io.LongWritable;
 
@@ -11,14 +13,14 @@ public class TAirportReducer extends Reducer<Text, IntWritable, Text, LongWritab
     @Override
     protected void reduce(TAirportKeyComparable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
        Iterator iter = values.iterator();
-       float min = 0;
+       float min = 1000000;
        float max = 0;
        float sum = 0;
        int count = 0;
        while(iter.hasNext()) {
            iter.next();
            float delay = Float.parseFloat(iter.toString());
-           count++;
+           if (min = Float.min(min, ))
        }
        context.write(key, new LongWritable(count));
     }
