6e6505510806f131bfbf838fa509f0f2565ca5d5
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-10-18 01:07:11 +0300

    Scripted auto-commit on change (2019-10-18 01:07:11) by gitwatch.sh

--- reduceSideJoin/TKeyGroupingComparator.java
+++ reduceSideJoin/TKeyGroupingComparator.java
@@ -9,12 +9,6 @@ public class TKeyGroupingComparator extends WritableComparator
     public TKeyGroupingComparator() {
     }
 
-    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-        int a = readInt(b1, s1);
-        int b = readInt(b2, s2);
-        return Integer.compare(a, b);
-    }
-
     public int	compare(WritableComparable a, WritableComparable b) {
         TAirportKeyComparable x = (TAirportKeyComparable) a;
         TAirportKeyComparable y = (TAirportKeyComparable) b;
