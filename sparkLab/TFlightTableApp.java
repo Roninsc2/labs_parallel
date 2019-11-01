22073af3a1e7ce8ece5be995ba509fbcd8f68082
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 21:07:54 +0300

    Scripted auto-commit on change (2019-11-01 21:07:54) by gitwatch.sh

--- sparkLab/TFlightTableApp.java
+++ sparkLab/TFlightTableApp.java
@@ -6,10 +6,10 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class TFlightTableApp {
     private static final int AIRPORT_ID_CLMN = 0;
     private static final int AIRPORT_NAME_CLMN = 1;
-    private static final int ORIGIN_ID_CLMN = 0;
-    private static final int DEST_ID_CLMN = 0;
-    private static final int DELAY_ID_CLMN = 0;
-    private static final int CANCEL_ID_CLMN = 0;
+    private static final int ORIGIN_ID_CLMN = 11;
+    private static final int DEST_ID_CLMN = 14;
+    private static final int DELAY_ID_CLMN = 18;
+    private static final int CANCEL_ID_CLMN = 19;
 
 
     public static void main(String[] args) {
