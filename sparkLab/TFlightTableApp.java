b43247620a8ad76f4fcd38cdde1a3a8e158ab6e3
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 19:51:37 +0300

    Scripted auto-commit on change (2019-11-01 19:51:37) by gitwatch.sh

--- sparkLab/TFlightTableApp.java
+++ sparkLab/TFlightTableApp.java
@@ -4,6 +4,6 @@ import org.apache.spark.SparkConf;
 
 public class TFlightTableApp {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf();
+        SparkConf conf = new SparkConf().setAppName("lab3");
     }
 }
