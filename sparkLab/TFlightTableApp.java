28e1d144f595271cd3733304e74f799006440e2f
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 21:22:25 +0300

    Scripted auto-commit on change (2019-11-01 21:22:25) by gitwatch.sh

--- sparkLab/TFlightTableApp.java
+++ sparkLab/TFlightTableApp.java
@@ -15,7 +15,7 @@ public class TFlightTableApp {
     private static final String FLIGHTS_FILE = "664600583_T_ONTIME_sample.csv";
     private static final String OUTPUT_FILE = "lab3_output";
 
-    private int getId(String[] val, int clmn) {
+    private Integer getId(String[] val, int clmn) {
 
     }
 
