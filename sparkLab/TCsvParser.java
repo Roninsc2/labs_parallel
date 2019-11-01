641e2bcd18d81fbcad5704af521ba114e53531c6
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 21:02:51 +0300

    Scripted auto-commit on change (2019-11-01 21:02:51) by gitwatch.sh

--- sparkLab/TCsvParser.java
+++ sparkLab/TCsvParser.java
@@ -4,16 +4,5 @@ public class TCsvParser {
         private static final String COMMA = ",";
         private static final String QUOTES = "\"";
 
-        public TCsvParser(String text) {
-            String fileData = text.toString();
-            String[] lines = fileData.split(DELIMITER);
-            String idStr = lines[ID_CLMN];
-            if (idStr.length() == ID_LEN) {
-                id = Integer.parseInt(idStr.substring(BEGIN_ID_INDX, END_ID_INDX));
-            } else {
-                id = INVALID_ID;
-            }
-        }
-    }
-
+        static String[] getColumns(String line)
 }
