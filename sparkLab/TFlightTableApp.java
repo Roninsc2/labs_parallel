84a04de731f5755fd9f773ccb7e0aaf953b72e77
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 21:10:38 +0300

    Scripted auto-commit on change (2019-11-01 21:10:38) by gitwatch.sh

--- sparkLab/TFlightTableApp.java
+++ sparkLab/TFlightTableApp.java
@@ -1,6 +1,7 @@
 package sparkLab;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class TFlightTableApp {
@@ -15,9 +16,11 @@ public class TFlightTableApp {
     private static final String OUTPUT_FILE = "lab3_output";
 
 
-    
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> airportFile = sc.textFIle(AIRPORT_FILE);
     }
 }
