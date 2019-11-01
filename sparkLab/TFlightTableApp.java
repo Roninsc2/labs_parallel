88593db122e473024d087a4bb220e9a31bd0c274
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 21:32:29 +0300

    Scripted auto-commit on change (2019-11-01 21:32:29) by gitwatch.sh

--- sparkLab/TFlightTableApp.java
+++ sparkLab/TFlightTableApp.java
@@ -42,8 +42,8 @@ public class TFlightTableApp {
                 map(TCsvParser::getColumns).
                 filter(val -> !val[CANCEL_ID_CLMN].equals(FIRST_LINE_CHECK_FLIGHTS));
 
-        final Broadcast<Map<Integer, String>> airportBroadcast = airportClmn.
-                mapToPair(val -> new Tuple2<>(getId(val, AIRPORT_ID_CLMN), getName(val))).collectAsMap();
+        final Broadcast<Map<Integer, String>> airportBroadcast = sc.broadcast(airportClmn.
+                mapToPair(val -> new Tuple2<>(getId(val, AIRPORT_ID_CLMN), getName(val))).collectAsMap());
 
     }
 }
