93687c50ff2852478a025bd30a9d46abddd75536
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 19:47:12 +0300

    Scripted auto-commit on change (2019-11-01 19:47:12) by gitwatch.sh

--- sparkLab/TFlightTableApp.java
+++ sparkLab/TFlightTableApp.java
@@ -1,50 +1,7 @@
 package sparkLab;
 
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import reduceSideJoin.*;
-
-import java.io.IOException;
-
 public class TFlightTableApp {
-    package reduceSideJoin;
-
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import java.io.IOException;
-
-    public class TDelayApp {
-        public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-            if (args.length != 3) {
-                System.err.println("Usage: TDelayApp <input airport path> <input flight path> <output path>");
-                System.exit(-1);
-            }
-            Job job = Job.getInstance();
-            job.setJarByClass(reduceSideJoin.TDelayApp.class);
-            job.setJobName("Airports delay");
-            MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TAirportMapper.class);
-            MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, TFlightMapper.class);
-            FileOutputFormat.setOutputPath(job, new Path(args[2]));
-            job.setGroupingComparatorClass(TKeyGroupingComparator.class);
-            job.setPartitionerClass(TAirportIdPartitioner.class);
-            job.setReducerClass(TAirportReducer.class);
-            job.setMapOutputKeyClass(TAirportKeyComparable.class);
-            job.setOutputKeyClass(IntWritable.class);
-            job.setOutputValueClass(Text.class);
-            job.setNumReduceTasks(1);
-            System.exit(job.waitForCompletion(true) ? 0 : 1);
-        }
+    public static void main(String[] args) {
+        
     }
-
-
 }
