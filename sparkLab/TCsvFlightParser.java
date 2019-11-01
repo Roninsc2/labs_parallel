160b2e95aebffda73b52bb5c7ec4a32f48154b1d
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 20:39:18 +0300

    Scripted auto-commit on change (2019-11-01 20:39:18) by gitwatch.sh

--- sparkLab/TCsvFlightParser.java
+++ sparkLab/TCsvFlightParser.java
@@ -1,4 +1,30 @@
 package sparkLab;
 
 public class TCsvFlightParser {
+    private int id;
+    private String delay;
+    private static final int INVALID_ID = -1;
+    private static final String DELIMITER = ",";
+    private static final int ID_LEN = 5;
+    private static final int ID_CLMN = 14;
+    private static final int DELAY_CLMN = 17;
+
+    public TCsvFlightReader(Text text) {
+        String[] lines = text.toString().split(DELIMITER);
+        String idStr = lines[ID_CLMN];
+        delay = lines[DELAY_CLMN];
+        if (idStr.length() == ID_LEN && delay.length() > 0) {
+            id = Integer.parseInt(idStr);
+        } else {
+            id = INVALID_ID;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDelay() {
+        return delay;
+    }
 }
