5eedcab3256ad5e1ec75eaefbd6fa2fb2fef8129
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 22:38:51 +0300

    Scripted auto-commit on change (2019-11-01 22:38:51) by gitwatch.sh

--- sparkLab/TFlightTableApp.java
+++ sparkLab/TFlightTableApp.java
@@ -59,6 +59,7 @@ public class TFlightTableApp {
         JavaPairRDD<Tuple2<Integer, Integer>, TFlightDataCalc> dataCalc = flightClmn.mapToPair(val -> new Tuple2<>(
                 new Tuple2<>(getId(val, ORIGIN_ID_CLMN), getId(val, DEST_ID_CLMN)),
                 new TFlightDataCalc(getDelay(val), getCancel(val)))).reduceByKey(TFlightDataCalc::calculate);
+        
 
         final Broadcast<Map<Integer, String>> airportBroadcast = sc.broadcast(airportClmn.
                 mapToPair(val -> new Tuple2<>(getId(val, AIRPORT_ID_CLMN), getName(val))).collectAsMap());
