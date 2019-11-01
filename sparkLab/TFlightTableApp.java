4cfd9944691c7d17e8af88fb97c76170d113e131
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 21:04:12 +0300

    Scripted auto-commit on change (2019-11-01 21:04:12) by gitwatch.sh

--- sparkLab/TFlightTableApp.java
+++ sparkLab/TFlightTableApp.java
@@ -4,6 +4,15 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class TFlightTableApp {
+
+
+    private static final int INVALID_ID = -1;
+    private static final String DELIMITER = ",";
+    private static final int ID_LEN = 7;
+    private static final int BEGIN_ID_INDX = 1;
+    private static final int END_ID_INDX = 6;
+    private static final int ID_CLMN = 0;
+    
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
