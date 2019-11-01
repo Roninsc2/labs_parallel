8f9ff102da72f499fd37a06aeef161792fcf6e5b
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-01 22:28:13 +0300

    Scripted auto-commit on change (2019-11-01 22:28:13) by gitwatch.sh

--- sparkLab/TFlightDataCalc.java
+++ sparkLab/TFlightDataCalc.java
@@ -36,6 +36,7 @@ public class TFlightDataCalc implements Serializable {
     public String toString() {
         int delayPart = delayCount/sumCount * 100;
         int cancelPart = cancelCount/sumCount * 100;
-        return "max delay: " + delay + " " +
+        return "| max delay: " + delay + "| delay part: " + delayPart
+                + "| cancel part: " + cancelPart;
     }
 }
