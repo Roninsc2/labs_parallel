591da961380abbe557f315e0abf9f9af2903070d
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 19:01:20 +0300

    Scripted auto-commit on change (2019-11-15 19:01:20) by gitwatch.sh

--- akkaTestJS/THttpRouter.java
+++ akkaTestJS/THttpRouter.java
@@ -4,4 +4,5 @@ import akka.http.javadsl.server.AllDirectives;
 
 public class THttpRouter extends AllDirectives {
     private static final String TEST_STARTED = "TEST STARTED";
+    private static final String TEST_PATH = "test";
 }
