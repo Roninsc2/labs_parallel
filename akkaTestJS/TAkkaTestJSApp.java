b50df20d1f5936b4542200df6772b65e8ecc7c6c
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 19:17:16 +0300

    Scripted auto-commit on change (2019-11-15 19:17:16) by gitwatch.sh

--- akkaTestJS/TAkkaTestJSApp.java
+++ akkaTestJS/TAkkaTestJSApp.java
@@ -1,11 +1,14 @@
 package akkaTestJS;
 
+import akka.actor.ActorSystem;
+
 public class TAkkaTestJSApp {
 
     public static final String ROOT_ACTOR = "rootActor";
     public static final String STORAGE_ACTOR = "storageActor";
 
+    private static final String ACTOR_SYSTEM = "test-js";
     public static void main(String[] args) throws Exception {
-        
+        ActorSystem sys = ActorSystem.create()
     }
 }
