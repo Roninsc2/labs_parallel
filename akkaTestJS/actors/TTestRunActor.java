0377d64804636eb2b1e4d77a090d2b8032e274c8
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 18:52:05 +0300

    Scripted auto-commit on change (2019-11-15 18:52:05) by gitwatch.sh

--- akkaTestJS/actors/TTestRunActor.java
+++ akkaTestJS/actors/TTestRunActor.java
@@ -17,7 +17,7 @@ public class TTestRunActor extends AbstractActor {
     private static final String ERROR_RUNTIME = "RUNTIME ERROR";
     private static final String PATH_TO_STORAGE_ACTOR = "/user/" + TAkkaTestJSApp.ROOT_ACTOR + "/" + TAkkaTestJSApp.STORAGE_ACTOR;
 
-    public Receive createRecive() {
+    public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TPacketTest.class, val -> {
                     ScriptEngine engine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
