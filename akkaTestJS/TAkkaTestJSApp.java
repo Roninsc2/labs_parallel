c2aec5074116ba55654d2c84a8c075ba189897c3
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 23:24:58 +0300

    Scripted auto-commit on change (2019-11-15 23:24:58) by gitwatch.sh

--- akkaTestJS/TAkkaTestJSApp.java
+++ akkaTestJS/TAkkaTestJSApp.java
@@ -23,7 +23,7 @@ public class TAkkaTestJSApp {
     private static final String ACTOR_SYSTEM = "test-js";
     private static final String HOST = "localhost";
     private static final int PORT = 8080;
-    private static final String START_MESSAGE = "Server starts, http://" + HOST + ":" + PORT + "/\n";
+    private static final String START_MESSAGE = "Server started, http://" + HOST + ":" + PORT + "/\n";
 
     public static void main(String[] args) throws Exception {
         ActorSystem sys = ActorSystem.create(ACTOR_SYSTEM);
