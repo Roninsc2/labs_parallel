1dea9bbeffd3c16a9d057bc4155b00c17c2e0429
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 18:08:08 +0300

    Scripted auto-commit on change (2019-11-15 18:08:08) by gitwatch.sh

--- akkaTestJS/actors/TStorageActor.java
+++ akkaTestJS/actors/TStorageActor.java
@@ -5,8 +5,7 @@ import akka.japi.pf.ReceiveBuilder;
 
 public class TStorageActor extends AbstractActor {
 
-    private static final String ERROR_404 = "ERROR 404 : NOT FOUND";
-
+    private static final String ERROR_404 = "ERROR 404 : RESULT NOT FOUND";
 
     public Receive createRecive() {
 
