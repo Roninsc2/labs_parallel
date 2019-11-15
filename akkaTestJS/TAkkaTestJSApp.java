c892b319cf19de32fd7bfe650a7f97c2f694e045
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 19:16:24 +0300

    Scripted auto-commit on change (2019-11-15 19:16:24) by gitwatch.sh

--- akkaTestJS/TAkkaTestJSApp.java
+++ akkaTestJS/TAkkaTestJSApp.java
@@ -4,4 +4,8 @@ public class TAkkaTestJSApp {
 
     public static final String ROOT_ACTOR = "rootActor";
     public static final String STORAGE_ACTOR = "storageActor";
+
+    public static void main(String[] args) throws Exception {
+        
+    }
 }
