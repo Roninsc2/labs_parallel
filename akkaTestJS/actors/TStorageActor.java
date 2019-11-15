96c359b8aec877afd1b8fa96ea37b93d87c59cdd
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 18:09:09 +0300

    Scripted auto-commit on change (2019-11-15 18:09:09) by gitwatch.sh

--- akkaTestJS/actors/TStorageActor.java
+++ akkaTestJS/actors/TStorageActor.java
@@ -2,11 +2,15 @@ package akkaTestJS.actors;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import akkaTestJS.testsJSON.TTestResult;
+
+import java.util.*;
 
 public class TStorageActor extends AbstractActor {
 
     private static final String ERROR_404 = "ERROR 404 : RESULT NOT FOUND";
 
+    private Map<String, List<TTestResult>> storage = new HashMap<>();
     public Receive createRecive() {
 
     }
