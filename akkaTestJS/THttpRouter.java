075efb689020e8f584481363f45ba7819b0b8a44
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 19:02:54 +0300

    Scripted auto-commit on change (2019-11-15 19:02:54) by gitwatch.sh

--- akkaTestJS/THttpRouter.java
+++ akkaTestJS/THttpRouter.java
@@ -8,4 +8,7 @@ public class THttpRouter extends AllDirectives {
     private static final String RESULT_PATH = "result";
     private static final String PARAMETER_PACKAGE_ID = "packageId";
     private static final int TIMEOUT = 5000;
+
+    THttpRouter(){
+    }
 }
