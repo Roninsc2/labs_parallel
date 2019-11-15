dae3268f7a958c5c80b15e02fa3de2062098b031
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 18:47:31 +0300

    Scripted auto-commit on change (2019-11-15 18:47:31) by gitwatch.sh

--- akkaTestJS/actors/TRootActor.java
+++ akkaTestJS/actors/TRootActor.java
@@ -1,9 +1,14 @@
 package akkaTestJS.actors;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akkaTestJS.TAkkaTestJSApp;
 
 public class TRootActor extends AbstractActor {
 
     private static final int INITIAL_POOL_SIZE = 3;
+
+    private ActorRef storageActor = getContext().actorOf(Props.create(TStorageActor.class), TAkkaTestJSApp.STORAGE_ACTOR);
 
 }
