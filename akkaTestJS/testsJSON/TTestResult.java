20722a218992f56b17af373b5c64d074ebc6656f
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 17:20:01 +0300

    Scripted auto-commit on change (2019-11-15 17:20:01) by gitwatch.sh

--- akkaTestJS/testsJSON/TTestResult.java
+++ akkaTestJS/testsJSON/TTestResult.java
@@ -29,7 +29,7 @@ public class TTestResult {
         name = n;
         isSuccess = successFlag;
         result = res;
-        expectedRes = exRes;
+        expectedResult = exRes;
         params = p;
     }
 
