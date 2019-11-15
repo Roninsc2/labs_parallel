c51f0d335461ec308bb87063918857ba755031c6
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 18:53:46 +0300

    Scripted auto-commit on change (2019-11-15 18:53:46) by gitwatch.sh

--- akkaTestJS/actors/TRootActor.java
+++ akkaTestJS/actors/TRootActor.java
@@ -7,20 +7,23 @@ import akka.japi.pf.ReceiveBuilder;
 import akka.routing.RoundRobinPool;
 import akkaTestJS.TAkkaTestJSApp;
 import akkaTestJS.packetJSON.TPacketTest;
+import akkaTestJS.testsJSON.TTest;
 
 public class TRootActor extends AbstractActor {
 
     private static final int INITIAL_POOL_SIZE = 3;
 
     private ActorRef storageActor = getContext().actorOf(Props.create(TStorageActor.class), TAkkaTestJSApp.STORAGE_ACTOR);
-    private ActorRef testActor = getContext().actorOf(
+    private ActorRef testRouter = getContext().actorOf(
             new RoundRobinPool(INITIAL_POOL_SIZE).props(Props.create(TTestRunActor.class))
     );
 
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TPacketTest.class, val -> {
-
+                    for (TTest test : val.getTests()) {
+                        test
+                    }
                 }).build();
     }
 
