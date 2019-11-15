b11b7e8142ec70bf9ea7666a14b0b4e9a56f146b
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 18:23:06 +0300

    Scripted auto-commit on change (2019-11-15 18:23:06) by gitwatch.sh

--- akkaTestJS/actors/TTestRunActor.java
+++ akkaTestJS/actors/TTestRunActor.java
@@ -4,7 +4,7 @@ import akka.actor.AbstractActor;
 
 public class TTestRunActor extends AbstractActor {
 
-
+    private static final String ENGINE_NAME = "nashorn";
     public Receive createRecive() {
 
     }
