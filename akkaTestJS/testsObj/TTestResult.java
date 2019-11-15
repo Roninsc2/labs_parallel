6c16a7b605e0991b0ee2b3680827b4809b8f97b2
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-15 15:31:42 +0300

    Scripted auto-commit on change (2019-11-15 15:31:42) by gitwatch.sh

--- akkaTestJS/testsObj/TTestResult.java
+++ akkaTestJS/testsObj/TTestResult.java
@@ -1,4 +1,9 @@
 package akkaTestJS.testsObj;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 public class TTestResult {
 }
