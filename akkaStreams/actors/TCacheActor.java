63815b2758489b02dca3efe48d357e74a9950d9a
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-30 01:04:12 +0300

    Scripted auto-commit on change (2019-11-30 01:04:12) by gitwatch.sh

--- akkaStreams/actors/TCacheActor.java
+++ akkaStreams/actors/TCacheActor.java
@@ -15,7 +15,7 @@ public class TCacheActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TPingPkt.class, val -> {
-                    Integer res = cache.getOrDefault(val.getUrl(), -1L);
+                    Integer res = cache.getOrDefault(val.getUrl(), -1);
                     sender().tell(new TPingPkt(val.getUrl(), res), self());
                 })
                 .match(TPongPkt.class, val -> {
