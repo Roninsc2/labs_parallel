684e73e23748d6789f8d2220669f690754b59494
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-30 00:22:31 +0300

    Scripted auto-commit on change (2019-11-30 00:22:31) by gitwatch.sh

--- akkaStreams/pingApp/TServer.java
+++ akkaStreams/pingApp/TServer.java
@@ -1,6 +1,10 @@
 package akkaStreams.pingApp;
 
+import akka.actor.ActorRef;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Dsl;
 public class TServer {
 
     private AsyncHttpClient client =  Dsl.asyncHttpClient();
+    private ActorRef actor;
 }
