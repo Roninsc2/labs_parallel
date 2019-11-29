bf088aa0c02ac854b4dc2cd379ba711d53935b10
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-30 00:25:34 +0300

    Scripted auto-commit on change (2019-11-30 00:25:34) by gitwatch.sh

--- akkaStreams/pingApp/TServer.java
+++ akkaStreams/pingApp/TServer.java
@@ -1,9 +1,13 @@
 package akkaStreams.pingApp;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.scaladsl.model.HttpResponse;
+import akka.stream.javadsl.Flow;
 import akkaStreams.actors.TCacheActor;
+import org.apache.http.HttpRequest;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.Dsl;
 public class TServer {
@@ -13,5 +17,12 @@ public class TServer {
 
     TServer(ActorSystem system) {
         actor = system.actorOf(Props.create(TCacheActor.class));
+    }
+
+    FLow<HttpRequest, HttpResponse, NotUsed> getFlow() {
+        return Flow.of(HttpRequest.class).map(val ->
+
+        )
+        .mapAsync()
     }
 }
