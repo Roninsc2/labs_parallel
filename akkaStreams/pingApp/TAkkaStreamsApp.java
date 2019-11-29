f52edb2c54bd936243c403117ffa19659c964d0c
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-11-30 01:36:22 +0300

    Scripted auto-commit on change (2019-11-30 01:36:22) by gitwatch.sh

--- akkaStreams/pingApp/TAkkaStreamsApp.java
+++ akkaStreams/pingApp/TAkkaStreamsApp.java
@@ -20,7 +20,7 @@ public class TAkkaStreamsApp {
     public static final String ROOT_ACTOR = "rootActor";
     public static final String STORAGE_ACTOR = "storageActor";
 
-    private static final String ACTOR_SYSTEM = "test-js";
+    private static final String ACTOR_SYSTEM = "ping";
     private static final String HOST = "localhost";
     private static final int PORT = 8080;
     private static final String START_MESSAGE = "Server started, http://" + HOST + ":" + PORT + "/\n";
