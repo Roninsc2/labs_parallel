d8f6873561f7e50611800323d285f9ce637a0664
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-14 02:35:21 +0300

    Scripted auto-commit on change (2019-12-14 02:35:21) by gitwatch.sh

--- akkaZookeeper/actor/TStorageConfigActor.java
+++ akkaZookeeper/actor/TStorageConfigActor.java
@@ -21,4 +21,8 @@ public class TStorageConfigActor extends AbstractActor {
                 })
                 .build();
     }
+
+    private String getServer() {
+        String url = serverList[new Random().nextInt(serverList.length)];
+    }
 }
