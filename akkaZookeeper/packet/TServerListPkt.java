48452e4b779643a42285cb3e40ad762b59f6d5a9
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-14 02:23:05 +0300

    Scripted auto-commit on change (2019-12-14 02:23:05) by gitwatch.sh

--- akkaZookeeper/packet/TServerListPkt.java
+++ akkaZookeeper/packet/TServerListPkt.java
@@ -2,4 +2,8 @@ package akkaZookeeper.packet;
 
 public class TServerListPkt {
     private String[] serverList;
+
+    public TServerListPkt(String[] serverList) {
+        this.serverList = serverList;
+    }
 }
