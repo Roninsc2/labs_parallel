cf970f96d8fc5b6858253afc3c4ce973e065c73d
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-14 02:30:53 +0300

    Scripted auto-commit on change (2019-12-14 02:30:53) by gitwatch.sh

--- akkaZookeeper/actor/TStorageConfigActor.java
+++ akkaZookeeper/actor/TStorageConfigActor.java
@@ -8,4 +8,16 @@ import java.util.Random;
 public class TStorageConfigActor extends AbstractActor {
 
     private String[] serverList;
+
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(TPingPkt.class, val -> {
+                    Long res = cache.getOrDefault(val.getUrl(), -1L);
+                    sender().tell(new TPongPkt(val.getUrl(), res), self());
+                })
+                .match(TPongPkt.class, val -> {
+                    cache.put(val.getUrl(), val.getAvrgPongTime());
+                })
+                .build();
+    }
 }
