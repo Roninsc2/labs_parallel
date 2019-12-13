737466cdb38a7c73179c6707d6e7551317ac081a
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-14 02:57:13 +0300

    Scripted auto-commit on change (2019-12-14 02:57:13) by gitwatch.sh

--- akkaZookeeper/TServiceZK.java
+++ akkaZookeeper/TServiceZK.java
@@ -28,6 +28,8 @@ public class TServiceZK {
                     watchServers();
                 }
             });
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
