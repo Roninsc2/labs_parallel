edb4833f096a95174fbef794bb740e4e4374b4d4
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-14 02:52:03 +0300

    Scripted auto-commit on change (2019-12-14 02:52:03) by gitwatch.sh

--- akkaZookeeper/TServiceZK.java
+++ akkaZookeeper/TServiceZK.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 public class TServiceZK {
     private static final String ZK_HOST_PORT = "127.0.0.1:2181";
     private static final int TIMEOUT = 3000;
+    private static final String ROOT = "/servers"
 
     private  ZooKeeper zk;
     private ActorRef actor;
