307257215ad6b4d8b1b5669174cb63baeb6da4a8
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-14 03:08:51 +0300

    Scripted auto-commit on change (2019-12-14 03:08:51) by gitwatch.sh

--- akkaZookeeper/TServer.java
+++ akkaZookeeper/TServer.java
@@ -20,7 +20,6 @@ import org.asynchttpclient.Dsl;
 
 import java.time.Duration;
 import java.util.Collections;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 public class TServer extends AllDirectives {
