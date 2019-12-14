1a37c0ac8d6f879df6de5e7a4618b5480310287e
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-14 03:29:23 +0300

    Scripted auto-commit on change (2019-12-14 03:29:22) by gitwatch.sh

--- akkaZookeeper/TServer.java
+++ akkaZookeeper/TServer.java
@@ -7,6 +7,7 @@ import akka.actor.Props;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.*;
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 import akkaZookeeper.packet.TServerPkt;
 import org.apache.zookeeper.KeeperException;
@@ -33,60 +34,14 @@ public class TServer extends AllDirectives {
         serviceZK.createServer(SERVER_URL + port);
     }
 
-    Flow<HttpRequest, HttpResponse, NotUsed> getFlow(ActorMaterializer materializer) {
-        return Flow
-                .of(HttpRequest.class)
-                .map(val -> {
-                    Query requestQuery = val.getUri().query();
-                    String url = requestQuery.getOrElse(URL_NAME, "");
-                    int count = Integer.parseInt(requestQuery.getOrElse(COUNT_NAME, "-1"));
+    public Route createRoute() {
+        return get(() ->
+            parameter(URL_NAME, url ->
+                    parameter(COUNT_NAME, count -> {
 
-                    return new TPingPkt(url, count);
-                })
-                .mapAsync(PARALLELISM, ping -> Patterns.ask(actor, ping, Duration.ofMillis(TIMEOUT))
-                        .thenCompose(pong -> {
-                            TPongPkt cachePongPkt = (TPongPkt)pong;
-
-                            return cachePongPkt.getAvrgPongTime() == -1
-                                    ? pingExecute(ping, materializer)
-                                    : CompletableFuture.completedFuture(cachePongPkt);
-                        }))
-                .map(pong -> {
-                    actor.tell(pong , ActorRef.noSender());
-
-                    return HttpResponse
-                            .create()
-                            .withStatus(StatusCodes.OK)
-                            .withEntity(
-                                    HttpEntities.create(
-                                            pong.getUrl() + " " + pong.getAvrgPongTime()
-                                    )
-                            );
-                });
-    }
-
-    private CompletionStage<TPongPkt> pingExecute(TPingPkt ping, ActorMaterializer materializer) {
-        return Source.from(Collections.singletonList(ping))
-                .toMat(pingSink(), Keep.right())
-                .run(materializer)
-                .thenApply(summaryTime -> new TPongPkt(
-                        ping.getUrl(),
-                        summaryTime / ping.getCount())
-                );
-    }
-
-    private Sink<TPingPkt, CompletionStage<Long>> pingSink() {
-        return Flow.<TPingPkt>create()
-                .mapConcat(ping -> Collections.nCopies(ping.getCount(), ping.getUrl()))
-                .mapAsync(PARALLELISM, url -> {
-                    long startTime = System.currentTimeMillis();
-
-                    return client
-                            .prepareGet(url)
-                            .execute()
-                            .toCompletableFuture()
-                            .thenApply(pong -> System.currentTimeMillis() - startTime);
-                })
-                .toMat(Sink.fold(0L, Long::sum), Keep.right());
+                        }
+                    )
+            )
+        );
     }
 }
