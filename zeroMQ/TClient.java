97cced56a4e26feb06e997542fae5698a10d5154
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-21 00:46:25 +0300

    Scripted auto-commit on change (2019-12-21 00:46:25) by gitwatch.sh

--- zeroMQ/TClient.java
+++ zeroMQ/TClient.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 import static zeroMQ.TConfig.*;
 
 public class TClient {
+    private static final String ERROR = "STORAGE ERROR";
 
     public static void main(String[] args) {
         try {
 
-        } catch () {
-            
+        } catch (ZMQException e) {
+            System.out.println(E);
+
         }
     }
 }
