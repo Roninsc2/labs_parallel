e4283a6d2b86bfb4f0fdb9f6e0894c6124e48e9a
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-21 01:00:10 +0300

    Scripted auto-commit on change (2019-12-21 01:00:10) by gitwatch.sh

--- zeroMQ/TClient.java
+++ zeroMQ/TClient.java
@@ -34,7 +34,6 @@ public class TClient {
                     if (request == null) {
                         break;
                     }
-
                     System.out.println(request.toString());
                     request.destroy();
                 }
