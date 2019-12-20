8f9ab0b607fd7f4f17d02d02c4f018124553d8af
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-21 00:33:48 +0300

    Scripted auto-commit on change (2019-12-21 00:33:48) by gitwatch.sh

--- zeroMQ/TStorage.java
+++ zeroMQ/TStorage.java
@@ -54,7 +54,7 @@ public class TStorage {
             int pos = Integer.parseInt(contentArr[1]);
             String value = cache.get(pos);
             msg.pollLast();
-            msg.addLast(DONE);
+            msg.addLast(PUT_DONE);
         }
         if (contentArr[0].equals(PUT_CMD)) {
 
