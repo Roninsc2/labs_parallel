21cbb8414385f9fda340be9bdbefadfd57ad71aa
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-21 01:05:18 +0300

    Scripted auto-commit on change (2019-12-21 01:05:18) by gitwatch.sh

--- zeroMQ/TProxy.java
+++ zeroMQ/TProxy.java
@@ -1,4 +1,8 @@
 package zeroMQ;
 
 public class TProxy {
+
+    private static final String PROXY_ERROR = "THERE WAS AN ERROR WITH A PROXY PLEASE RETRY";
+    private static final String NO_CACHE_ERROR = "NO CACHE AVAILABLE";
+    private static final String INVALID_DATA = "INVALID DATA";
 }
