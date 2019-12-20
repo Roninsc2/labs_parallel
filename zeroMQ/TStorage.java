87a4a5eb5a8af0d865c676900ab1bf14265d973d
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-21 00:33:39 +0300

    Scripted auto-commit on change (2019-12-21 00:33:39) by gitwatch.sh

--- zeroMQ/TStorage.java
+++ zeroMQ/TStorage.java
@@ -10,7 +10,7 @@ public class TStorage {
     private static int leftBound, rightBound;
     private static final int BACKEND = 0;
     private static final String ERROR = "STORAGE ERROR";
-    private static final String DONE = "DONE";
+    private static final String PUT_DONE = "DONE";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
