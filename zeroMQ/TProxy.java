37de6e648e195a878cc2f99a6e8a20a01b5e01fe
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-21 01:38:53 +0300

    Scripted auto-commit on change (2019-12-21 01:38:53) by gitwatch.sh

--- zeroMQ/TProxy.java
+++ zeroMQ/TProxy.java
@@ -27,11 +27,84 @@ public class TProxy {
             Map<ZFrame, TCacheMeta> commutator = new HashMap<>();
             long time = System.currentTimeMillis();
             while (!Thread.currentThread().isInterrupted()) {
-                
+                poller.poll(1);
+                time = cleanCache(commutator, time);
+
+                if (poller.pollin(FRONTEND_MSG)) {
+                    if (processFrontendMessage(backend, frontend, commutator)) break;
+                }
+
+                if (poller.pollin(BACKEND_MSG)) {
+                    ZMsg msg = ZMsg.recvMsg(backend);
+                    if (msg == null) {
+                        break;
+                    }
+                    if (msg.getLast().toString().contains(HEARTBEAT_COMMAND)) {
+                        if (!commutator.containsKey(msg.getFirst())) {
+                            ZFrame data = msg.getLast();
+                            String[] fields = data.toString().split(DELIMITER);
+                            CacheMeta tmp = new CacheMeta(
+                                    fields[1],
+                                    fields[2],
+                                    System.currentTimeMillis()
+                            );
+                            commutator.put(msg.getFirst().duplicate(), tmp);
+                            System.out.println("New cache -> " + msg.getFirst() + " " + tmp.getLeftBound() + " " + tmp.getRightBound());
+                        }else{
+                            commutator.get(msg.getFirst().duplicate()).setTime(System.currentTimeMillis());
+                        }
+                    } else {
+                        System.out.println("NO HEARTBEAT ->" + msg);
+                        msg.pop();
+                        msg.send(frontend);
+                    }
+                }
             }
         } catch (ZMQException e) {
-            System.out.println(ERROR);
+            System.out.println(PROXY_ERROR);
             e.printStackTrace();
         }
+    }
+
+    private static boolean processFrontendMessage(ZMQ.Socket backend, ZMQ.Socket frontend, Map<ZFrame, CacheMeta> commutator) {
+        ZMsg msg = ZMsg.recvMsg(frontend);
+        if (msg == null) {
+            return true;
+        }
+        if (commutator.isEmpty()) {
+            sendError(frontend, msg, NO_CACHE_ERROR);
+        } else {
+            String[] data = msg.getLast().toString().split(DELIMITER);
+            if (data[0].equals(GET_COMMAND)) {
+                for (Map.Entry<ZFrame, CacheMeta> map : commutator.entrySet()) {
+                    if (map.getValue().isIntersect(data[1])) {
+                        ZFrame cacheFrame = map.getKey().duplicate();
+                        msg.addFirst(cacheFrame);
+                        msg.send(backend);
+                    }
+                }
+            } else {
+                if (data[0].equals(PUT_COMMAND)) {
+                    for (Map.Entry<ZFrame, CacheMeta> map : commutator.entrySet()) {
+                        if (map.getValue().isIntersect(data[1])) {
+                            ZMsg tmp = msg.duplicate();
+                            ZFrame cacheFrame = map.getKey().duplicate();
+                            tmp.addFirst(cacheFrame);
+                            tmp.send(backend);
+                        }
+                    }
+                } else {
+                    sendError(frontend, msg, INVALID_DATA);
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void sendError(ZMQ.Socket frontend, ZMsg msg, String noCacheError) {
+        ZMsg errMsg = new ZMsg();
+        errMsg.add(noCacheError);
+        errMsg.wrap(msg.getFirst());
+        errMsg.send(frontend);
     }
 }
