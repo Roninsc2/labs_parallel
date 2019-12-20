f08a012eba067473c1d27e3fdbbdaf170c7cc80b
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-20 22:31:41 +0300

    Scripted auto-commit on change (2019-12-20 22:31:41) by gitwatch.sh

--- zeroMQ/TConfig.java
+++ zeroMQ/TConfig.java
@@ -5,7 +5,7 @@ public class TConfig {
     public static final String FRONTEND_SOCKET = "tcp://localhost:2052";
     public static final String HEARTBEAT_CMD = "Heartbeat";
     public static final String GET_CMD = "GET";
-    public static final String PUT_COMMAND = "PUT";
+    public static final String PUT_CMD = "PUT";
     public static final String DELIMITER = " ";
     public static final int BACKEND_MSG = 1;
     public static final int FRONTEND_MSG = 0;
