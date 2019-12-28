17c7d5bc1ea2567116cc78d8fdf613aff8a441fd
Виктор Фадеев <main_tuppa@rnd.stcnet.ru>
2019-12-28 06:17:44 +0300

    Scripted auto-commit on change (2019-12-28 06:17:44) by gitwatch.sh

--- zeroMQ/TProxy.java
+++ zeroMQ/TProxy.java
@@ -67,15 +67,6 @@ public class TProxy {
     }
 
     private static boolean processFrontendGet(ZMQ.Socket backend, ZMQ.Socket frontend, Map<ZFrame, TCacheMeta> commutator) {
-        ZMsg msg = ZMsg.recvMsg(frontend);
-        if (msg == null) {
-            return true;
-        }
-        if (commutator.isEmpty()) {
-            sendError(frontend, msg, NO_CACHE_ERROR);
-        } else {
-            String[] data = msg.getLast().toString().split(DELIMITER);
-            if (data[0].equals(GET_CMD)) {
                 for (Map.Entry<ZFrame, TCacheMeta> map : commutator.entrySet()) {
                     if (map.getValue().isIntersect(data[1])) {
                         ZFrame cacheFrame = map.getKey().duplicate();
@@ -83,22 +74,6 @@ public class TProxy {
                         msg.send(backend);
                     }
                 }
-            } else {
-                if (data[0].equals(PUT_CMD)) {
-                    for (Map.Entry<ZFrame, TCacheMeta> map : commutator.entrySet()) {
-                        if (map.getValue().isIntersect(data[1])) {
-                            ZMsg tmp = msg.duplicate();
-                            ZFrame cacheFrame = map.getKey().duplicate();
-                            tmp.addFirst(cacheFrame);
-                            tmp.send(backend);
-                        }
-                    }
-                } else {
-                    sendError(frontend, msg, INVALID_DATA);
-                }
-            }
-        }
-        return false;
     }
 
     private static void processFrontendPut(ZMsg msg, ZMQ.Socket backend, Map<ZFrame, TCacheMeta> commutator) {
